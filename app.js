8c016a8d9906eb3c191e0d76e3353b519fbfa6c3
SujaiKrishnaK <62582629+SujaiKrishnaK@users.noreply.github.com>
2020-10-22 11:50:39 +0530

    .gitinore added

--- app.js
+++ app.js
@@ -70,6 +70,12 @@ app.get("/posts/:topic",function(req,res){
   });
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
